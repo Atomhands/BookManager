326656ef2735136ce567d68aa8d85b1df1f2dca4
atomhands <13272766+atomhands@user.noreply.gitee.com>
2023-09-03 17:34:30 +0800

    Merge remote-tracking branch 'origin/master'

--- README.md
+++ README.md
@@ -5,11 +5,34 @@
 图书管理系统servlet工程
 
 #### 软件架构
-软件架构说明
+软件架构说明  
 
 
 #### 使用说明
 
 1. 此工程使用的是作者的本地数据库(oracle)，作者qq  2262152973@qq.com
 
+使用需要更改配置数据库连接池 
+
+3. 管理员表
+CREATE table TB_ADMIN(
+admin_Id  varchar2(50) primary key ,
+account varchar2(50),
+password varchar2(50),
+name varchar2(10),
+phone varchar2(16),
+create_Time timestamp,
+role_Name varchar2(10)
+);
+
+4. 图书表
+   CREATE table TB_BOOK_INFO(
+   book_Id  varchar2(50) primary key ,
+   book_Name varchar2(50),
+   publisher varchar2(50),
+   author varchar2(10),
+   book_Type varchar2(16),
+   remain varchar2(10)
+   );
+   INSERT INTO TB_BOOK_INFO values (1,'斗罗大陆','江苏','唐家三少','小说',50);
 
--- src/main/java/com/niehao/filter/EncodingFilter.java
+++ src/main/java/com/niehao/filter/EncodingFilter.java
@@ -7,24 +7,31 @@ import java.io.IOException;
 public class EncodingFilter implements Filter {
 
     private final String name = "encode";
-    private String encode;
+    String encode = null;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        encode = filterConfig.getInitParameter(name);// 获取 参数
+        encode = filterConfig.getInitParameter("encode");
+        //encode = filterConfig.getInitParameter(name);// 获取 参数
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // System.out.println("设置编码");
-        request.setCharacterEncoding(encode);
-        // response.setCharacterEncoding(encode);
-        chain.doFilter(request, response);
+        if (encode != null){
+            request.setCharacterEncoding(encode);
+            response.setContentType("text/html;charset="+encode);
+            chain.doFilter(request, response);
+        }
+
+        //request.setCharacterEncoding(encode);
+
     }
 
     @Override
     public void destroy() {
         // NO
+        encode = null;
     }
 
 }
